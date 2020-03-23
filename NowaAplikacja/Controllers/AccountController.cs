e6e1304d15a296b80e7bcd84c783fe430f9116bf
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-23 15:40:40 +0100

    Assign permission during registration

--- NowaAplikacja/Controllers/AccountController.cs
+++ NowaAplikacja/Controllers/AccountController.cs
@@ -155,6 +155,7 @@ namespace NowaAplikacja.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    UserManager.AddToRole(user.Id, "Student");
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // Aby uzyskać więcej informacji o sposobie włączania potwierdzania konta i resetowaniu hasła, odwiedź stronę https://go.microsoft.com/fwlink/?LinkID=320771
