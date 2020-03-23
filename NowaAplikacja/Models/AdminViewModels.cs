34ab3b4ea4820385a2bac2639069e5cd16c41425
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-23 15:44:29 +0100

    Create AdminViewModels class

---
+++ NowaAplikacja/Models/AdminViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NowaAplikacja.Models
+{
+    public class AdminUserViewModel
+    {
+        public string UserName { get; set; }
+        public string RankName { get; set; }
+        public string UserId { get; set; }
+        public string UserFullName { get; set; }
+        public string RankId { get; set; }
+    }
+
+}
