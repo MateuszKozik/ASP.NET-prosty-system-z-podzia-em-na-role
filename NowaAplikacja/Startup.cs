4e266c535a6a5f85d30038e09d61501ffb7d3e62
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-23 15:19:31 +0100

    Init new repository

---
+++ NowaAplikacja/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NowaAplikacja.Startup))]
+namespace NowaAplikacja
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
